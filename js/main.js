766f5fa0ca9f9ee0025a41ba57fd2bfe1c62d117
nicolo fossati <nicofossa01@gmail.com>
2023-03-09 15:08:43 +0100

    BONUS 1 COMPLETO

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@
     BONUS 2:
     Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
     BONUS 3:
-    Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
+    Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay. 
 */
 
 const images = [
